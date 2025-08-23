ce650d891d665b56ca524c7f5c086b4a39b7e43d
hurtatisz17 <alejohurtatis2005@gmail.com>
2025-08-22 23:29:54 -0500

    version02

---
+++ menuAdaptable.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  const menuToggle = document.getElementById('menu-toggle');
+
+  const navList = document.getElementById('nav-list');
+
+  menuToggle.addEventListener('click', function () {
+
+    navList.classList.toggle('active');
+  });
+});
